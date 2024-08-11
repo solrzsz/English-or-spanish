c579bb438cb7c0b815df8d8679ae79a8ce665170
solrzsz <solrr057@gmail.com>
2024-08-10 23:49:55 -0300

    new

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    window.addEventListener('load', function () {
+        var audio = document.getElementById('gayrs');
+        audio.muted = false;
+        audio.play();
+    });
+});
